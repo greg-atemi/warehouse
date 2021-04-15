461aa5ee60e325a71cf8263ff61aae7217cefcd5
Mark Chemweno <mchemweno27@gmail.com>
2021-04-15 15:02:30 +0300

    'Goods'

--- Warehouse/DB.cs
+++ Warehouse/DB.cs
@@ -1,50 +1,41 @@
-﻿using System.Data.SQLite;
+﻿using System;
+using System.Data.SQLite;
 using System.IO;
 
 namespace Warehouse
 {
-	public class Db
-	{
-		public SQLiteConnection Connection;
+    public class Db
+    {
+        public SQLiteConnection Connection;
 
-		public Db()
-		{
-			Connection = new SQLiteConnection("Data Source=warehouse_database.sqlite3");
-			if (!File.Exists("./warehouse_database.sqlite3"))
-			{
-				SQLiteConnection.CreateFile("warehouse_database.sqlite3");
-				System.Console.WriteLine("new database created");
-				/*
-				 * @Mark I have not yet implemented the logic
-				 * to create the schema if no database is found.
-				 * But below I have provided the schema for the
-				 * 'Client' & 'Cube' tables if you need them(because you need them)^_^
-				 */
-				
-				/*
-				 * +++ CLIENT TABLE SCHEMA +++
-				 */
-				
-				// CREATE TABLE "Client" (
-				// 	"id"	INTEGER NOT NULL,
-				// 	"name"	TEXT NOT NULL,
-				// 	"email"	TEXT NOT NULL,
-				// 	PRIMARY KEY("id" AUTOINCREMENT)
-				// );
-				
-				
-				
-				/*
-				 * +++ CUBE TABLE SCHEMA +++
-				 */
-				
-				// CREATE TABLE "Cube" (
-				// 	"id"	INTEGER NOT NULL,
-				// 	"name"	TEXT NOT NULL,
-				// 	"occupied"	INTEGER NOT NULL,
-				// 	PRIMARY KEY("id" AUTOINCREMENT)
-				// );
-			}
-		}
-	}
+        public Db()
+        {
+            Connection = new SQLiteConnection("Data Source=warehouse_database.sqlite3");
+
+            if (File.Exists("./warehouse_database.sqlite3")) return;
+
+            SQLiteConnection.CreateFile("warehouse_database.sqlite3");
+
+            Db myDb = new();
+            myDb.Connection.Open();
+
+            string[] commandTexts =
+            {
+                "CREATE TABLE Client (id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, email TEXT NOT NULL);",
+                "CREATE TABLE Cube (id	INTEGER NOT NULL PRIMARY KEY, name	TEXT NOT NULL, occupied	INTEGER NOT NULL);",
+                "CREATE TABLE Goods (id	INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, description TEXT NOT NULL, client_id	INTEGER NOT NULL, cube_id INTEGER NOT NULL, received_date INTEGER NOT NULL, released_date INTEGER, FOREIGN KEY (cube_id) REFERENCES Cube(id) ON UPDATE RESTRICT ON DELETE RESTRICT , FOREIGN KEY (client_id) REFERENCES Client(id) ON UPDATE RESTRICT ON DELETE RESTRICT )"
+            };
+
+            foreach (var commandString in commandTexts)
+            {
+                var command = new SQLiteCommand(myDb.Connection)
+                {
+                    CommandText = commandString
+                };
+                command.ExecuteNonQuery();
+            }
+            
+            myDb.Connection.Close();
+        }
+    }
 }
--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,12 +12,29 @@ namespace Warehouse
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
+        // defines for commandline output
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
+
+        private const int ATTACH_PARENT_PROCESS = -1;
+
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Good_record_form());
+            AttachConsole(ATTACH_PARENT_PROCESS);
+            
+            // Application.EnableVisualStyles();
+            // Application.SetCompatibleTextRenderingDefault(false);
+            // Application.Run(new Good_record_form());
+
+
+            // var nw = new Client("Canary", "mchemweno27@gmail.com");
+            // nw.Save();
+
+            // var nw = new Good("TV", "Electronic", DateTime.Now.Date, DateTime.Now.Date, 2,2);
+            // nw.Save();
+
         }
     }
 }
