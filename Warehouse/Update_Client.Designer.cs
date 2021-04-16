d55bb20f6c5d0958a4ca0699cb1fd29b43a79931
unknown <kevinogechi798@gmail.com>
2021-04-16 19:13:22 +0300

    update backend functionality for Client

--- Warehouse/Update_Client.Designer.cs
+++ Warehouse/Update_Client.Designer.cs
@@ -44,27 +44,30 @@ namespace Warehouse
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(451, 21);
+            this.label1.Location = new System.Drawing.Point(301, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.Size = new System.Drawing.Size(71, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Update Client";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(112, 174);
+            this.label2.Location = new System.Drawing.Point(75, 113);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(112, 227);
+            this.label3.Location = new System.Drawing.Point(75, 148);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Email";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -72,56 +75,65 @@ namespace Warehouse
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(112, 301);
+            this.label4.Location = new System.Drawing.Point(75, 196);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.Size = new System.Drawing.Size(78, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Phone Number";
             // 
             // name
             // 
-            this.name.Location = new System.Drawing.Point(254, 167);
+            this.name.Location = new System.Drawing.Point(169, 109);
+            this.name.Margin = new System.Windows.Forms.Padding(2);
             this.name.Name = "name";
-            this.name.Size = new System.Drawing.Size(223, 26);
+            this.name.Size = new System.Drawing.Size(150, 20);
             this.name.TabIndex = 4;
             // 
             // phone_number
             // 
-            this.phone_number.Location = new System.Drawing.Point(254, 301);
+            this.phone_number.Location = new System.Drawing.Point(169, 196);
+            this.phone_number.Margin = new System.Windows.Forms.Padding(2);
             this.phone_number.Name = "phone_number";
-            this.phone_number.Size = new System.Drawing.Size(223, 26);
+            this.phone_number.Size = new System.Drawing.Size(150, 20);
             this.phone_number.TabIndex = 5;
             // 
             // email
             // 
-            this.email.Location = new System.Drawing.Point(254, 227);
+            this.email.Location = new System.Drawing.Point(169, 148);
+            this.email.Margin = new System.Windows.Forms.Padding(2);
             this.email.Name = "email";
-            this.email.Size = new System.Drawing.Size(223, 26);
+            this.email.Size = new System.Drawing.Size(150, 20);
             this.email.TabIndex = 6;
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(218, 424);
+            this.btnUpdate.Location = new System.Drawing.Point(145, 276);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(170, 48);
+            this.btnUpdate.Size = new System.Drawing.Size(113, 31);
             this.btnUpdate.TabIndex = 7;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // get_email
             // 
-            this.get_email.Location = new System.Drawing.Point(516, 216);
+            this.get_email.Location = new System.Drawing.Point(344, 140);
+            this.get_email.Margin = new System.Windows.Forms.Padding(2);
             this.get_email.Name = "get_email";
-            this.get_email.Size = new System.Drawing.Size(170, 48);
+            this.get_email.Size = new System.Drawing.Size(113, 31);
             this.get_email.TabIndex = 8;
             this.get_email.Text = "Fetch";
             this.get_email.UseVisualStyleBackColor = true;
+            this.get_email.Click += new System.EventHandler(this.get_email_Click);
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(609, 424);
+            this.btnBack.Location = new System.Drawing.Point(406, 276);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(170, 48);
+            this.btnBack.Size = new System.Drawing.Size(113, 31);
             this.btnBack.TabIndex = 9;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -129,9 +141,9 @@ namespace Warehouse
             // 
             // Update_Client
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1182, 652);
+            this.ClientSize = new System.Drawing.Size(788, 424);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.get_email);
             this.Controls.Add(this.btnUpdate);
@@ -142,11 +154,11 @@ namespace Warehouse
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Update_Client";
             this.Text = "Update_Client";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
