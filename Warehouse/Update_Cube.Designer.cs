776a1ee3a89187156d32dec939354d76e5827549
mbevaliti <charles.liti@gmail.com>
2021-04-17 09:08:56 +0300

    error provider

--- Warehouse/Update_Cube.Designer.cs
+++ Warehouse/Update_Cube.Designer.cs
@@ -42,56 +42,45 @@ namespace Warehouse
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(327, 15);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(490, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(104, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Update Cube";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(47, 102);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(70, 157);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(51, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // name
             // 
-            this.name.Location = new System.Drawing.Point(161, 100);
-            this.name.Margin = new System.Windows.Forms.Padding(2);
+            this.name.Location = new System.Drawing.Point(242, 154);
             this.name.Name = "name";
-            this.name.Size = new System.Drawing.Size(133, 20);
+            this.name.Size = new System.Drawing.Size(198, 26);
             this.name.TabIndex = 2;
             // 
             // btnUpdate
             // 
-<<<<<<< HEAD
-            this.btnUpdate.Location = new System.Drawing.Point(230, 416);
-=======
-            this.btnUpdate.Location = new System.Drawing.Point(161, 205);
-            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
->>>>>>> 17b7412435d5a0b17aa350eebdd613877a544ddc
+            this.btnUpdate.Location = new System.Drawing.Point(308, 496);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(105, 29);
+            this.btnUpdate.Size = new System.Drawing.Size(158, 45);
             this.btnUpdate.TabIndex = 3;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
             // 
             // btnBack
             // 
-<<<<<<< HEAD
-            this.btnBack.Location = new System.Drawing.Point(642, 416);
-=======
-            this.btnBack.Location = new System.Drawing.Point(427, 205);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(2);
->>>>>>> 17b7412435d5a0b17aa350eebdd613877a544ddc
+            this.btnBack.Location = new System.Drawing.Point(782, 496);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(105, 29);
+            this.btnBack.Size = new System.Drawing.Size(158, 45);
             this.btnBack.TabIndex = 4;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -99,14 +88,10 @@ namespace Warehouse
             // 
             // get_name
             // 
-<<<<<<< HEAD
-            this.get_name.Location = new System.Drawing.Point(494, 196);
-=======
-            this.get_name.Location = new System.Drawing.Point(329, 100);
-            this.get_name.Margin = new System.Windows.Forms.Padding(2);
->>>>>>> 17b7412435d5a0b17aa350eebdd613877a544ddc
+            this.get_name.Location = new System.Drawing.Point(700, 243);
+            this.get_name.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.get_name.Name = "get_name";
-            this.get_name.Size = new System.Drawing.Size(105, 23);
+            this.get_name.Size = new System.Drawing.Size(158, 35);
             this.get_name.TabIndex = 5;
             this.get_name.Text = "Fetch";
             this.get_name.UseVisualStyleBackColor = true;
@@ -115,7 +100,8 @@ namespace Warehouse
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(71, 244);
+            this.label3.Location = new System.Drawing.Point(53, 317);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(68, 20);
             this.label3.TabIndex = 6;
@@ -123,33 +109,30 @@ namespace Warehouse
             // 
             // cube_id
             // 
-            this.cube_id.Location = new System.Drawing.Point(241, 244);
+            this.cube_id.Location = new System.Drawing.Point(242, 314);
+            this.cube_id.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cube_id.Name = "cube_id";
-            this.cube_id.Size = new System.Drawing.Size(197, 26);
+            this.cube_id.Size = new System.Drawing.Size(198, 26);
             this.cube_id.TabIndex = 7;
             // 
             // Update_Cube
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(1437, 674);
+            this.ClientSize = new System.Drawing.Size(1924, 1037);
             this.Controls.Add(this.cube_id);
             this.Controls.Add(this.label3);
-=======
-            this.ClientSize = new System.Drawing.Size(913, 438);
->>>>>>> 17b7412435d5a0b17aa350eebdd613877a544ddc
             this.Controls.Add(this.get_name);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.name);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Update_Cube";
             this.Text = "Update_Cube";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
